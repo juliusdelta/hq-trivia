3a805bee4604008b4a7bd5c7ee67d0a67c6ef83f
JD Gonzales <jd_gonzales@icloud.com>
2023-05-24 12:13:22 -0500

    Initial devise install & setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,7 @@
 
 require 'active_support/core_ext/integer/time'
 
+# rubocop:disable Metrics/BlockLength
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -43,6 +44,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -73,3 +76,4 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 end
+# rubocop:enable Metrics/BlockLength
