1e04c263bddc72d2f8b6150c32ccc331770970b5
JD Gonzales <jd_gonzales@icloud.com>
2023-05-24 13:11:06 -0500

    fix rubocop issues & tests for home controller

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,8 +1,11 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get home_index_url
+  test 'should get index' do
+    get root_path
+
     assert_response :success
   end
 end
