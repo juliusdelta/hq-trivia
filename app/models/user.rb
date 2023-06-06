3aab91c05a00e005ca162421f58c077c3ca86a2e
JD Gonzales <jd_gonzales@icloud.com>
2023-06-06 09:32:06 -0500

    Remove needless relation from user to game instance

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,5 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
-  belongs_to :game_instance, optional: true
   has_many :won_games, class_name: 'GameInstance', foreign_key: 'winner_id', dependent: :destroy, inverse_of: :winner
 end
